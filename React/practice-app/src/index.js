9d8dfde815813128c37cba2854fddd564e17e665
balasuriya <balasuriya.cs20@bitsathy.ac.in>
2023-03-03 13:47:23 +0530

    Stateful Components(Both Class and Functional), Handling Events and Conditional Rendering - Completed🚀

--- React/practice-app/src/index.js
+++ React/practice-app/src/index.js
@@ -5,23 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const person = {
-  name: "Bala",
-  noOfTimes: 0,
-  buttonPressed() {
-    person.noOfTimes++;
-    console.log(person.noOfTimes);
-  }
-};
-function tick() {
 
-  root.render(
-    <React.StrictMode>
-      <App props={person}/>
-    </React.StrictMode>
-  );
-}
-setInterval(tick, 1000);  
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
