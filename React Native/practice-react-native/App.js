882e7467a86d73bd465b241ff32c86db49eab2ee
balasuriya <balasuriya.cs20@bitsathy.ac.in>
2023-03-10 18:09:44 +0530

    Working on Navigation between pages🚀 and more on Hooks👨‍💻

--- React Native/practice-react-native/App.js
+++ React Native/practice-react-native/App.js
@@ -14,7 +14,6 @@ import {
 } from "react-native";
 
 import colors from "./app/config/colors";
-import AppButton from "./app/components/AppButton";
 import axios from "axios";
 import AppToDoList from "./app/components/AppToDoList";
 import AppBar from "./app/components/AppBar";
@@ -22,180 +21,14 @@ import AppFloatingActionButton from "./app/components/AppFloatingActionButton";
 import AppIcon from "./app/components/AppIcon";
 
 export default function App() {
-  const [taskInputController, settaskInputController] = useState("");
-  const [taskSearch, settaskSearch] = useState("");
-  const [todos, setTodos] = useState([]);
-  const [fetching, setFetching] = useState(true);
-  const [searching, setSearching] = useState(true);
-
-  useEffect(() => {
-    async function getTodos() {
-      const response = await axios(
-        "https://jsonplaceholder.typicode.com/todos"
-      );
-
-      let newTodos = [...response.data].slice(156, 178);
-      setTodos(newTodos);
-      setFetching(false);
-    }
-
-    getTodos();
-  }, []);
-
-  return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-    >
-      <View style={styles.ToDoContainer}>
-        <AppBar
-          size={30}
-          name={searching ? "search1" : "plus"}
-          iconColor="white"
-          barStyle={{
-            backgroundColor: colors.primary,
-            width: "95%",
-            marginTop: 10,
-            borderRadius: 25,
-            overflow: "hidden",
-          }}
-        >
-          {searching ? (
-            <TextInput
-              style={{ color: colors.white, flex: 1 }}
-              onChangeText={settaskSearch}
-              value={taskSearch}
-              placeholder={"Search..."}
-              placeholderTextColor={"#FFFFF0"}
-            />
-          ) : (
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <TextInput
-                autoFocus={!searching}
-                style={{ color: colors.white, flex: 0.8 }}
-                onChangeText={settaskInputController}
-                value={taskInputController}
-                placeholder={"What do you want to do?"}
-                placeholderTextColor={"#FAF9F6"}
-              />
-              <AppIcon
-                name="check"
-                size={65}
-                backgroundColor={colors.primary}
-                onPress={() => {
-                  if (taskInputController === "") return;
-                  let newTodos = [
-                    {
-                      userId: 1,
-                      id: todos.length + 1,
-                      title: taskInputController,
-                      completed: false,
-                    },
-                    ...todos,
-                  ];
-                  settaskInputController("");
-                  setTodos(newTodos);
-                  setSearching(!searching);
-                }}
-              />
-            </View>
-          )}
-        </AppBar>
-        <Text style={styles.title}>Todo Items</Text>
-        <FlatList
-          refreshing={fetching}
-          onRefresh={() => {
-            setFetching(false);
-            setTodos([...todos]);
-            setFetching(false);
-          }}
-          style={styles.list}
-          data={todos}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => {
-            return (
-              <AppToDoList
-                key={item.id}
-                onPressCheckBox={() => {
-                  let newTodos = todos.filter((todo) => {
-                    if (todo.id == item.id) todo.completed = !todo.completed;
-
-                    return todo;
-                  });
-
-                  setTodos(newTodos);
-                }}
-                onPressCross={() => {
-                  let newTodos = todos.filter((todo) => {
-                    if (todo.id != item.id) return todo;
-                  });
-
-                  setTodos(newTodos);
-                }}
-                data={item}
-              />
-            );
-          }}
-        />
-      </View>
-      <AppFloatingActionButton
-        backgroundColor={colors.primary}
-        name={!searching ? "search1" : "plus"}
-        size={65}
-        onPress={() => {
-          setSearching(!searching);
-        }}
-      />
-    </KeyboardAvoidingView>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
-  // appBar: {
-  //   padding: 10,
-  // },
-  button: {
-    alignSelf: "flex-start",
-    marginTop: 30,
-    marginLeft: 40,
-    width: "40%",
-    height: "4%",
-    borderRadius: 10,
-  },
   container: {
     flex: 1,
     backgroundColor: "#f8f4f4",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     alignItems: "center",
-  },
-  input: {
-    height: 60,
-    width: "80%",
-    marginTop: 30,
-    borderWidth: 1,
-    padding: 10,
-    borderColor: colors.grey,
-    borderRadius: 5,
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 700,
-    color: colors.white,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 600,
-    marginVertical: 15,
-  },
-  ToDoContainer: {
-    width: "100%",
-    alignItems: "center",
-    height: "100%",
   },
 });
