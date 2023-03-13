32e3691942119bce059e48dbe62428ae2b5ef9ac
balasuriya <balasuriya.cs20@bitsathy.ac.in>
2023-03-13 17:53:42 +0530

    Revised Some JS Concepts to use in Project

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,44 @@
-const person = {
-  name: "Bala",
-  talk() {
-    setTimeout(() => {
-        console.log(this.name + " is talking");
-    }, 0);
-  }
-};
+// Method Referencing
+// const person = {
+//   name: "Bala",
+//   talk() {
+//     setTimeout(() => {
+//       console.log(this.name + " is talking");
+//     }, 0);
+//   },
+// };
 
-console.log("Started");
-// person.talk();
+// console.log("Started");
+// // person.talk();
 
-const talking = person.talk;
+// const talking = () => {person.talk};
 
-talking();
+// talking();
+
+// Date Class
+// let x = Date.now();
+// let y = Math.random() * (1 - 0.996) + 0.996;
+// console.log(y);
+// console.log(new Date(x * y));
+// console.log(new Date().toString().slice(0, 24));
+
+//Object Dst
+// const func = () => {
+//   return [1, 2, 3];
+// };
+
+// const [a, b, c] = func();
+
+// // a = 6;
+
+// console.log(a + b + c);
+
+// Regex
+let re = "pa";
+let pattern = new RegExp(re, "g");
+
+let string = ["pattinson", "parrot", "not-pa", "pa"];
+
+string.filter((item) => {
+  console.log(item.match(pattern).length != 0);
+});
